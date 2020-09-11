fc22f308897583e9680fa53454dbf57336b8879c
cat-ask <duswltn00@naver.com>
2020-09-11 18:06:43 +0900

    수정사항중 변수명 snake_type이 지켜지도록했습니다, 하지만 확장자의 종류가 있는 리스트와 종류별로 묶인 리스트들을 담고 있는 리스트가 필요하다고 판단되어 그 부분은 수정하지 않았습니다.

--- Algorithm/sort.py
+++ Algorithm/sort.py
@@ -1,25 +1,25 @@
 import os
 
 #이름
-def sort_name(List,Type):
+def sort_name(names,reverse):
     # type
     # 오름차순 : false
     # 내림차순 : true
-    sort_list = List[:]
-    sort_list.sort(reverse=Type)
+    sort_list = names[:]
+    sort_list.sort(reverse=reverse)
     return sort_list
 
 #확장자
-def sort_exp(List):
+def sort_exp(names):
     exp_list = [] #확장자 리스트
     exp_content_list = [] #확장자로 구분한 파일 리스트
-    for l in range(len(List)):
-        exp = os.path.splitext(List[l])[1]
+    for l in range(len(names)):
+        exp = os.path.splitext(names[l])[1]
         if not (exp in exp_list):
             exp_list.append(exp)
             exp_content_list.append(list())
         
         exp_idx = exp_list.index(exp)
-        exp_content_list[exp_idx].append(List[l])
+        exp_content_list[exp_idx].append(names[l])
     
     return exp_list,exp_content_list
