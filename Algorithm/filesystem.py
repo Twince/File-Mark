20ad60361adc718bddd8ec3c122dbc45aff91acd
cat-ask <duswltn00@naver.com>
2020-10-27 21:22:31 +0900

    파일정보 반환함수 제작완료

--- Algorithm/filesystem.py
+++ Algorithm/filesystem.py
@@ -1,5 +1,7 @@
-import os
-import shutil
+import os       # 시스템관련 
+import datetime # 시간관련
+import time     # 시간관련
+import shutil   # 파일연산
 
 
 class FileSystem:
@@ -48,6 +50,51 @@ class FileSystem:
                 self.select_list.append(names)
 
     # 파일/디렉토리 정보
-    def file_info(self, names):
-        # more update
-        return names
+    def file_info(self):
+        names = "C:\\Users\\School\\Desktop\\File-Mark\\Algorithm"
+        # 가져오는 정보 {} 형태는 Dictionary(사전) 자료형이라고 한다. 자세한건 검색바람
+        # 이름
+        # 종류 (파일 : file / 디렉토리 : dir)
+        # 절대경로
+        # 포함하는 파일갯수 (default : 0)
+        # 포함하는 파일목록 (default : [])
+        # 만들어진 시간
+        # 최근 변경시간
+        # 최근 접근시간
+        # 파일크기 (단위 : 바이트)
+        # 확장자 (default : "")
+        infoList = {
+            "name" : "",
+            "type" : "",
+            "absRoot" : "",
+            "fileCount" : 0,
+            "fileList" : [],
+            "ctime" : "",
+            "mtime" : "",
+            "atime" : "",
+            "size" : 0,
+            "exp" : ""
+        }
+
+        # 이름이 비어있지않는경우 혹은 파일/디렉토리 가 아닌경우
+        if names != "" or (os.path.isdir(names) is False and os.path.isfile(names) is False):
+            infoList["name"] = os.path.basename(names)
+            infoList["type"] = "dir" if os.path.isdir(names) else "file"
+            infoList["absRoot"] = os.path.abspath(names)
+            infoList["fileCount"] = len(os.listdir(names)) if os.path.isdir(names) else 0
+            infoList["fileList"] = os.listdir(names) if os.path.isdir(names) else []
+            infoList["ctime"] = datetime.datetime.fromtimestamp(os.path.getctime(names)).strftime("%Y-%m-%d %H:%M:%S")
+            infoList["mtime"] = datetime.datetime.fromtimestamp(os.path.getmtime(names)).strftime("%Y-%m-%d %H:%M:%S")
+            infoList["atime"] = datetime.datetime.fromtimestamp(os.path.getatime(names)).strftime("%Y-%m-%d %H:%M:%S")
+            infoList["size"] = os.path.getsize(names)
+            infoList["exp"] = os.path.splitext(names)[1] if os.path.isfile(names) else ""
+        else :
+            msg = "ERROR : 파일이 선택되지않음"
+        
+        return infoList
+
+
+Fsystem = FileSystem(os.getcwd())
+info = Fsystem.file_info()
+for key, ans in info.items():
+    print(key, " : ",ans)
