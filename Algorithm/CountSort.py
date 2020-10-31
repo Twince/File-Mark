075620776a1469022d22e29a5fb5e74b0528c6f2
Eunhak Lee <lee@enak.kr>
2020-10-31 23:02:31 +0900

    style: pep8

--- Algorithm/CountSort.py
+++ Algorithm/CountSort.py
@@ -1,41 +1,46 @@
 import os
 
+
 class AAlignment:
-  def __init__(self, directory_route):
-    self.dir_list = os.listdir(directory_route)
+    def __init__(self, directory_route):
+        self.dir_list = os.listdir(directory_route)
 
-  # 디렉토리들의 파일 개수에 따라 정렬해 나온 리스트를 반환
-  def sort_file_Count(self, rever=True):
-    sort_dict = dict()
+    # 디렉토리들의 파일 개수에 따라 정렬해 나온 리스트를 반환
+    def sort_file_Count(self, rever=True):
+        sort_dict = dict()
 
-    for li in self.dir_list:
-      if os.path.isdir(os.path.abspath(li)):
-        file_size = len(os.listdir(li))
-      else :
-        file_size = -1
+        for li in self.dir_list:
+            if os.path.isdir(os.path.abspath(li)):
+                file_size = len(os.listdir(li))
+            else:
+                file_size = -1
 
-      sort_dict[li] = file_size
+            sort_dict[li] = file_size
 
-    sort_list = sorted(sort_dict, key=lambda k: sort_dict[k], reverse=rever)
+        sort_list = sorted(sort_dict, key=lambda k: sort_dict[k], reverse=rever)
 
-    return sort_list
+        return sort_list
 
-  def sort_file_dirfile(self):
-    df_list = [[],[]]
+    def sort_file_dirfile(self):
+        df_list = [[], []]
 
-    for l in self.dir_list: 
-      if os.path.isdir(l): df_list[0].append(l)
-      elif os.path.isfile(l): df_list[1].append(l)
+        for l in self.dir_list:
+            if os.path.isdir(l):
+                df_list[0].append(l)
+            elif os.path.isfile(l):
+                df_list[1].append(l)
 
-    return df_list
-      
-# test
-file_A = AAlignment(os.getcwd())
+        return df_list
 
-print("\n포함된 파일 갯수에 따라 정렬된 리스트 :")
-print(file_A.sort_file_Count())
 
-print("\n파일 리스트 :")
-print(file_A.sort_file_dirfile()[0])
-print("\n디렉토리 리스트 :")
-print(file_A.sort_file_dirfile()[1])
+if __name__ == '__main__':
+    # test
+    file_A = AAlignment(os.getcwd())
+
+    print("\n포함된 파일 갯수에 따라 정렬된 리스트 :")
+    print(file_A.sort_file_Count())
+
+    print("\n파일 리스트 :")
+    print(file_A.sort_file_dirfile()[0])
+    print("\n디렉토리 리스트 :")
+    print(file_A.sort_file_dirfile()[1])
