dd79a4c62f53657b00f53c4d6910b875e8d41fc3
fkdlalsem <as5869bc@gmail.com>
2020-11-29 23:17:21 +0900

    modi

--- Qt5 - UI/FileMark.py
+++ Qt5 - UI/FileMark.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import Qt
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 form_class = uic.loadUiType("new start.ui")[0]
 
+
 #화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
